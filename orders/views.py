b20ad793809ccd812527073cd6f1382f03efb396
ArranBailey <arran630@gmail.com>
2026-03-11 22:31:19 +0000

    added initial order and orderitem models

---
+++ orders/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import redirect
+from .cart import Cart
+
+
+def add_to_cart(request, product_id):
+
+    cart = Cart(request)
+
+    cart.add(product_id)
+
+    return redirect("view_cart")
