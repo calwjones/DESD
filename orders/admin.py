41a19e2d4a098c818db664d5d72e4fe8c90db7fc
ArranBailey <arran630@gmail.com>
2026-03-11 23:48:01 +0000

    Update to view cart logic

--- orders/admin.py
+++ orders/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from .models import Order, OrderItem
 
 
-admin.site.register(Order)
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+
+
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderItem)
