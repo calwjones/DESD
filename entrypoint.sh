ab0b0b4c0f5d8948b0b1b0218ca9a7fe5c953f56
LeiYe7 <lei.ye7@outlook.com>
2026-03-13 10:20:32 +0000

    added auto load on container startup (entrypoint.sh)

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,9 @@
 echo "Running migrations..."
 python manage.py migrate --noinput
 
+echo "Loading fixtures...(demo data)"
+python manage.py loaddata accounts
+python manage.py loaddata producers
+python manage.py loaddata products
+
 exec "$@"
