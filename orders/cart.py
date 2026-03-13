dec6f64084253165549c9a8eab6241f2557dcb2d
calwjones <147060628+calwjones@users.noreply.github.com>
2026-03-13 11:14:58 +0000

    complete cart functionality - remove, update qty, prod subtotals

--- orders/cart.py
+++ orders/cart.py
@@ -9,21 +9,16 @@ class Cart:
 
         self.cart = cart
 
-
     def add(self, product_id, quantity=1):
-
         product_id = str(product_id)
 
         if product_id not in self.cart:
             self.cart[product_id] = 0
 
         self.cart[product_id] += quantity
-
         self.save()
 
-
     def remove(self, product_id):
-
         product_id = str(product_id)
 
         if product_id in self.cart:
@@ -31,6 +26,16 @@ class Cart:
 
         self.save()
 
+    def update(self, product_id, quantity):
+        product_id = str(product_id)
+
+        if quantity > 0:
+            self.cart[product_id] = quantity
+        else:
+            self.remove(product_id)
+            return
+
+        self.save()
 
     def save(self):
         self.session.modified = True
--- orders/views.py
+++ orders/views.py
@@ -1,48 +1,58 @@
-from django.shortcuts import redirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
 from .cart import Cart
 from products.models import Product
 
 
 def add_to_cart(request, product_id):
-
     cart = Cart(request)
-
     cart.add(product_id)
-
     return redirect("view_cart")
 
 
+def remove_from_cart(request, product_id):
+    cart = Cart(request)
+    cart.remove(product_id)
+    return redirect("view_cart")
+
+
+def update_cart(request, product_id):
+    if request.method == "POST":
+        quantity = int(request.POST.get("quantity", 1))
+        cart = Cart(request)
+        cart.update(product_id, quantity)
+    return redirect("view_cart")
 
 
 def view_cart(request):
-
     cart = Cart(request)
 
     cart_items = []
+    producer_subtotals = {}
     total = 0
 
     for product_id, quantity in cart.cart.items():
-
-        product = Product.objects.get(id=product_id)
-        
+        product = get_object_or_404(Product, id=product_id)
         subtotal = product.price * quantity
-
         total += subtotal
 
+        # Track subtotals per producer
+        producer_name = product.producer.username
+        if producer_name not in producer_subtotals:
+            producer_subtotals[producer_name] = 0
+        producer_subtotals[producer_name] += subtotal
 
         cart_items.append({
             "product": product,
             "quantity": quantity,
-            "subtotal": subtotal
+            "subtotal": subtotal,
         })
 
-      
     return render(
         request,
         "orders/cart.html",
         {
             "cart_items": cart_items,
-            "total": total
-        }
+            "total": total,
+            "producer_subtotals": producer_subtotals,
+        },
     )
